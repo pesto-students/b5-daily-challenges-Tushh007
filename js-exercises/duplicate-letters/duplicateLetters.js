6b0d0140cb336a4e37aadc441392eca982cd9ae9
Tushar Langer <tushar.langer@joulon.com>
2021-01-05 21:55:28 +0530

    improved comments

--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -6,14 +6,18 @@ function duplicateLetters(...args) {
   let max = Number.NEGATIVE_INFINITY;
   let second_max = Number.NEGATIVE_INFINITY;
 
-  // counting frequency of each letter
-  // if the letter is there in the frequency counter - increment the value by 1 - if not - add it in the counter with value = 1
+  /* counting frequency of each letter
+     if the letter is there in the frequency counter - increment the value by 1
+     if not - add it in the counter with value = 1
+  */
   for (const letter of args[0]) {
     frequencyCounter[letter] = (frequencyCounter[letter] || 0) + 1;
   }
 
-  // finding max and second maximum repeated letter
-  // if the value of the frequency is greater than current max - max equals greater value and secomd max equals the last max
+  /* finding max and second maximum repeated letter
+     if the value of the frequency is greater than current max - 
+     max equals greater value and second max equals the last max
+   */
   for (const value in frequencyCounter) {
     if (frequencyCounter[value] > max) {
       max = frequencyCounter[value];
