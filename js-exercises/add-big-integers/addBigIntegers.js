a160a180d0cfc6086490128fd30a3acc07f947a7
Tushar Langer <tushar.langer@joulon.com>
2021-01-05 20:30:51 +0530

    work in progress

--- js-exercises/add-big-integers/addBigIntegers.js
+++ js-exercises/add-big-integers/addBigIntegers.js
@@ -1,3 +1,10 @@
-function addBigIntegers(intString) {}
+function addBigIntegers(intString) {
+  let sum = 0;
+  for (let number of intString.split('\n')) {
+    sum += parseFloat(number);
+  }
+
+  return sum.toString();
+}
 
 export { addBigIntegers };
