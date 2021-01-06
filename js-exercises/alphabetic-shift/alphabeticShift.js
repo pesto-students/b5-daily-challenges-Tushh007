c699fab27980ef61e5cd0b6659f74fca07114f86
Tushar Langer <tushar.langer@joulon.com>
2021-01-06 20:51:56 +0530

    implementing class feedback

--- js-exercises/alphabetic-shift/alphabeticShift.js
+++ js-exercises/alphabetic-shift/alphabeticShift.js
@@ -1,10 +1,10 @@
-const alphabeticShift = (input) => {
+const alphabeticShift = (input, shiftBy) => {
   // initializing final resultant string
   let result = '';
 
   // parsing through the string and using ASCII values for incrimenting letters
   for (const letter of input)
-    result += String.fromCharCode(letter.charCodeAt() + 1);
+    result += String.fromCharCode(letter.charCodeAt() + shiftBy);
 
   return result;
 };
--- js-exercises/alphabetic-shift/alphabeticShift.test.js
+++ js-exercises/alphabetic-shift/alphabeticShift.test.js
@@ -2,19 +2,19 @@ import { alphabeticShift } from './alphabeticShift';
 
 describe('Alphabetic Shift Test Cases', () => {
   it('returns a shifted string', () => {
-    expect(alphabeticShift('Pseudopseudohypoparathyroidism'))
+    expect(alphabeticShift('Pseudopseudohypoparathyroidism', 1))
       .toEqual('Qtfvepqtfvepizqpqbsbuizspjejtn');
 
-    expect(alphabeticShift('Floccinaucinihilipilification'))
+    expect(alphabeticShift('Floccinaucinihilipilification', 1))
       .toEqual('Gmpddjobvdjojijmjqjmjgjdbujpo');
 
-    expect(alphabeticShift('Floccinaucinihilipilification'))
+    expect(alphabeticShift('Floccinaucinihilipilification', 1))
       .toEqual('Gmpddjobvdjojijmjqjmjgjdbujpo');
 
-    expect(alphabeticShift('Antidisestablishmentarianism'))
+    expect(alphabeticShift('Antidisestablishmentarianism', 1))
       .toEqual('Boujejtftubcmjtinfoubsjbojtn');
 
-    expect(alphabeticShift('supercalifragilisticexpialidocious'))
+    expect(alphabeticShift('supercalifragilisticexpialidocious', 1))
       .toEqual('tvqfsdbmjgsbhjmjtujdfyqjbmjepdjpvt');
   });
 });
