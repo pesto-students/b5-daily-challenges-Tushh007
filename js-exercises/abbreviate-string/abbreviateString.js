2d0a199e0c666ce0fd91f0fd15c292d9e19e00ac
Tushar Langer <tushar.langer@joulon.com>
2021-01-06 20:09:34 +0530

    code reviewed

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,8 +1,16 @@
-import { isString } from 'util';
-
 function abbreviateString(str) {
+  // splitting string into words
   const wordsInString = str.split(' ');
-  return wordsInString[0] + ' ' + wordsInString[wordsInString.length - 1][0].toUpperCase() + '.';
+
+  // initializing first word
+  const firstWord = wordsInString[0];
+
+  // initializing last ward and uppercasing it
+  const abbreviatedLastWord =
+    wordsInString[wordsInString.length - 1][0].toUpperCase() + '.';
+
+  // concatinating the words for final abbriviated string
+  return `${firstWord} ${abbreviatedLastWord}`;
 }
 
 export { abbreviateString };
