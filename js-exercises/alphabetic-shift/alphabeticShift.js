6655765bb5aaa6705b3d6439b9ceb9cf9419a10e
Tushar Langer <tushar.langer@joulon.com>
2021-01-06 20:23:31 +0530

    solved

--- js-exercises/alphabetic-shift/alphabeticShift.js
+++ js-exercises/alphabetic-shift/alphabeticShift.js
@@ -1,5 +1,10 @@
 const alphabeticShift = input => {
-  return input;
+  let result = '';
+  for (const letter of input) {
+    result += String.fromCharCode(letter.charCodeAt() + 1)
+  }
+
+  return result;
 };
 
 export { alphabeticShift };
