7c4201a788481c4143dc8d88e9b9dca6199a0a93
Tushar Langer <tushar.langer@joulon.com>
2021-01-06 19:59:43 +0530

    solved

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,5 +1,8 @@
-import { isString } from "util";
+import { isString } from 'util';
 
-function abbreviateString(str) {}
+function abbreviateString(str) {
+  const wordsInString = str.split(' ');
+  return wordsInString[0] + ' ' + wordsInString[wordsInString.length - 1][0].toUpperCase() + '.';
+}
 
 export { abbreviateString };
